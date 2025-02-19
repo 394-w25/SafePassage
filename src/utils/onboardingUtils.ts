183021c80d3b329d32da1b862fb1a4d205b0102e
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2025-02-19 16:30:07 -0600

    fix: user age type and display issue

--- src/utils/onboardingUtils.ts
+++ src/utils/onboardingUtils.ts
@@ -13,3 +13,17 @@ export const isValidPhoneNumber = (phone: string): boolean => {
 export const formatPhoneNumber = (phone: string): string => {
   return phone.replace(/[()\s-]/g, '')
 }
+
+/**
+ *
+ * @param dateOfBirth {string | undefined} - Date of birth in string format (YYYY-MM-DD)
+ * @returns {string | number} - Age of the user or 'N/A' if dateOfBirth is undefined
+ */
+export const calculateAge = (dateOfBirth: string | undefined): string | number => {
+  if (dateOfBirth === undefined) {
+    return 'N/A'
+  }
+  const dobYear = new Date(dateOfBirth).getFullYear()
+  const currentYear = new Date().getFullYear()
+  return currentYear - dobYear
+}
