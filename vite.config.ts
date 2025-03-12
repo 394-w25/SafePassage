dba5fb377d07bbb2aec0838b7448f50d4ceeb3e5
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2025-03-12 18:03:34 -0500

    chore: clean up and update README

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
-import { VitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
@@ -10,35 +9,6 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
     checker({ typescript: true }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'apple-touch-icon.png', 'favicon-96x96.png', 'favicon-192x192.png', 'favicon-512x512.png'],
-      manifest: {
-        name: 'SafePassage',
-        short_name: 'SafePassage',
-        description: 'A brand new app called SafePassage',
-        theme_color: '#E4E0EE',
-        background_color: '#ffffff',
-        icons: [
-          {
-            src: 'favicon-96x96.png',
-            sizes: '96x96',
-            type: 'image/png',
-          },
-          {
-            src: 'favicon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'favicon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
   ],
   server: {
     open: true,
@@ -54,8 +24,8 @@ export default defineConfig({
           firebaseApp: ['firebase/app'],
           firebaseAuth: ['firebase/auth'],
           firebaseFirestore: ['firebase/firestore'],
-          state: ['zustand', 'zod', 'immer'],
-          chore: ['sonner', '@zl-asica/react'],
+          state: ['zustand', 'immer'],
+          chore: ['sonner', '@zl-asica/react', 'dayjs'],
           qrcode: ['qrcode.react'],
         },
       },
